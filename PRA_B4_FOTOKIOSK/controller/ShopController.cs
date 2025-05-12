ef2d3df362b294520f73d8d842eb5db82ab47993
BeppieOostkust <beppieoostkust@gmail.com>
2025-05-12 11:28:14 +0200

    First steps

--- PRA_B4_FOTOKIOSK/controller/ShopController.cs
+++ PRA_B4_FOTOKIOSK/controller/ShopController.cs
@@ -6,6 +6,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
+using System.Windows.Documents;
 
 namespace PRA_B4_FOTOKIOSK.controller
 {
@@ -14,16 +16,33 @@ namespace PRA_B4_FOTOKIOSK.controller
 
         public static Home Window { get; set; }
 
+        public static List<(string productName, double productPrice)> productList = new List<(string, double)>
+        {
+            ("Foto 10x15", 2.55),
+            ("Foto 20x15", 4.55),
+            ("Foto 30x15", 6.55),
+            ("Foto 40x15", 8.55),
+            ("Foto 50x20", 10.55),
+        };
+
         public void Start()
         {
             // Stel de prijslijst in aan de rechter kant.
-            ShopManager.SetShopPriceList("Prijzen:\nFoto 10x15: €2.55");
+            ShopManager.SetShopPriceList("Prijzen:\n");
 
             // Stel de bon in onderaan het scherm
             ShopManager.SetShopReceipt("Eindbedrag\n€");
 
             // Vul de productlijst met producten
-            ShopManager.Products.Add(new KioskProduct() { Name = "Foto 10x15" });
+            foreach(var product in productList)
+            {
+                ShopManager.AddShopPriceList($"\n{product.productName}: {product.productPrice}");
+                ShopManager.Products.Add(new KioskProduct()
+                {
+                    Name = product.productName,
+                    Price = product.productPrice
+                });
+            }
             
             // Update dropdown met producten
             ShopManager.UpdateDropDownProducts();
